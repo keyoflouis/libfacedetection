84d5ded29ec94a71bc3668a29be4452489a734c4
keyoflouis <132821554+keyoflouis@users.noreply.github.com>
2024-04-03 12:56:55 +0800

    Windows删掉了环境变量，一些总结

--- src/facedetectcnn.cpp
+++ src/facedetectcnn.cpp
@@ -144,7 +144,6 @@ inline float dotProduct(const float * p1, const float * p2, int num)
     }
    sum = _mm512_reduce_add_ps(sum_float_x16);
 #elif defined(_ENABLE_CUDA)
-    cuda_dotProduct();
     for(int i = 0; i < num; i++)
     {
         sum += (p1[i] * p2[i]);
